0a68c0ccfe5eed510604aa98dab74cec37475b6f
RapboyGao <atgao74201@qq.com>
2025-08-09 16:25:45 +0800

    2025.08.09 refactor(nuxt): 重构nuxt配置以使用nuxt-gin-tools
    将自定义的nuxt配置迁移到nuxt-gin-tools提供的默认配置
    删除旧的配置文件并更新依赖版本

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
+import { createDefaultConfig } from 'nuxt-gin-tools/src/nuxt-config';
+import type { NuxtConfig } from 'nuxt/config';
 import { defineNuxtConfig } from 'nuxt/config';
-import config from './config';
+import SERVER_CONFIG from './server.config.json';
+import { BASE_PATH } from './vue/composables/api/base';
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const config = createDefaultConfig({
+  apiBasePath: BASE_PATH,
+  serverConfig: SERVER_CONFIG,
+}) as NuxtConfig;
+
 export default defineNuxtConfig({
   ...config,
 });
