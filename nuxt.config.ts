60cd5b6480125a1641c934515964e5c655badd22
RapboyGao <atgao74201@qq.com>
2025-08-10 12:14:23 +0800

    2025.08.10 chore: 更新依赖并移除无用配置
    升级 nuxt-gin-tools 版本至 0.1.15
    移除 nuxt.config.ts 中多余的 NuxtConfig 类型声明
    删除不再使用的 .air.toml 配置文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import { createDefaultConfig } from 'nuxt-gin-tools/src/nuxt-config';
-import type { NuxtConfig } from 'nuxt/config';
 import { defineNuxtConfig } from 'nuxt/config';
 import SERVER_CONFIG from './server.config.json';
 import { BASE_PATH } from './vue/composables/api/base';
@@ -7,7 +6,7 @@ import { BASE_PATH } from './vue/composables/api/base';
 const config = createDefaultConfig({
   apiBasePath: BASE_PATH,
   serverConfig: SERVER_CONFIG,
-}) as NuxtConfig;
+});
 
 export default defineNuxtConfig({
   ...config,
