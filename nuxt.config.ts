b123e3686481481ed417b9325ba1933dda1fcd55
RapboyGao <atgao74201@qq.com>
2026-02-10 10:59:30 +0800

    chore: 更新依赖并简化nuxt配置
    移除不再需要的API基础路径和WebSocket代理配置
    升级nuxt-gin-tools至0.2.12版本

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,8 @@
 import { createDefaultConfig } from 'nuxt-gin-tools/src/nuxt-config';
 import { defineNuxtConfig } from 'nuxt/config';
 import SERVER_CONFIG from './server.config.json';
-import { API_BASE_PATH } from './vue/composables/api-base';
-
 const config = createDefaultConfig({
-  apiBasePath: API_BASE_PATH,
   serverConfig: SERVER_CONFIG,
-  wsProxyBasePaths: ['/ws-go'],
 });
 
 export default defineNuxtConfig({
