9edc0d8b1c33948dbfa8a82b440d64dd21fd1632
RapboyGao <atgao74201@qq.com>
2026-02-10 11:00:05 +0800

    style: 在nuxt.config.ts中添加空行以提升可读性

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { createDefaultConfig } from 'nuxt-gin-tools/src/nuxt-config';
 import { defineNuxtConfig } from 'nuxt/config';
 import SERVER_CONFIG from './server.config.json';
+
 const config = createDefaultConfig({
   serverConfig: SERVER_CONFIG,
 });
